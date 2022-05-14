1a30932b1a80bd192ab4f81a058fa1089001d4fc
Cipri Dorbe <cipridorbe@gmail.com>
2022-05-14 18:51:44 -0300

    Added lsp to cmp

--- lua/cipri/cmp.lua
+++ lua/cipri/cmp.lua
@@ -52,6 +52,8 @@ cmp.setup{
 	},
 	-- Keymaps in keymaps.lua
 	sources = {
+		{name = "nvim_lsp"},
+		{name = "nvim_lua"},
 		{name = "luasnip"},
 		{name = "buffer"},
 		{name = "path"},
@@ -59,10 +61,11 @@ cmp.setup{
 	formatting = {
 		fields = {"kind", "abbr", "menu"},
 		format = function(entry, vim_item)
-			-- Kind icons
+			-- Icons
 			vim_item.kind = string.format("%s", icons[vim_item.kind])
-			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
 			vim_item.menu = ({
+				nvim_lsp = "[LSP]",
+				nvim_lua = "[NVIM_LUA]",
 				luasnip = "[Snip]",
 				buffer = "[Buff]",
 				path = "[Path]",
